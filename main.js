4f87127f1421f146973aa55686955ddb7fe40852
amirakhaled2027 <155392660+amirakhaled2027@users.noreply.github.com>
2024-01-06 02:06:32 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+
+let x = 'amira';
+let y = 'saad';
+console.log(x == y);
+
+// let askEmail = prompt('enter your email');
+// let email = "amirakhaled2027@gmail.com";
+// console.log(askEmail.toLowerCase().trim() == email);
+
+
+//to transform uppercase to lowercase and the opposite
+let a = '       omar    ';
+console.log(a.toUpperCase());
+console.log(a.toLowerCase());
+console.log(a.length);
+console.log(a.trim().length);
